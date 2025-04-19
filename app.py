df4b63126957bf27413944dcf9f292a0e776c90f
Ishac79 <56246816+Ishac79@users.noreply.github.com>
2025-04-19 11:34:16 +0200

    Fetch a list of breweries from Brewery API

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def root():
-    return 'Welcome to the Brewery App backend.'
+OPEN_BREWERY_API_URL = "https://api.openbrewerydb.org/v1/breweries"
 
-if __name__ == '__main__':
+@app.route("/")
+def root():
+    return "Welcome to the Brewery App backend."
+
+@app.route("/breweries")
+def get_breweries():
+    """
+    Fetches a list of breweries from the Open Brewery DB API.
+    Returns a JSON response with the list of breweries.
+    """
+    try:
+        response = requests.get(OPEN_BREWERY_API_URL)
+    except requests.exceptions.RequestException as e:
+        return {"error": str(e)}, 500
+    return response.json()
+
+if __name__ == "__main__":
     app.run(debug=True)
